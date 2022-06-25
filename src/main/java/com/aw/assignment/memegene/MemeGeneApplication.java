f0e91085fa81eaeea76f06d6b43a87fdf52c8ed8
Aleksander Wardzinski <aleks.wardzinski@gmail.com>
2022-06-25 11:46:16 +0200

    add checkstyle

--- src/main/java/com/aw/assignment/memegene/MemeGeneApplication.java
+++ src/main/java/com/aw/assignment/memegene/MemeGeneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MemeGeneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemeGeneApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(MemeGeneApplication.class, args);
+    }
 
 }
--- src/test/java/com/aw/assignment/memegene/MemeGeneApplicationTests.java
+++ src/test/java/com/aw/assignment/memegene/MemeGeneApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MemeGeneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
